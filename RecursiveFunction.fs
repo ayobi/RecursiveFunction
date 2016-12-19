dd6b7cb3edb460e015f3cdb1107d2d08cb16c5dc
Aaron O'Brien <obrien.aaron.g@gmail.com>
2016-12-19 16:40:00 -0500

    Add files via upload

---
+++ RecursiveFunction.fs
@@ -0,0 +1,15 @@
+﻿module RecursiveFunction
+
+let rec length = function //rec indicates recursion
+    | [] -> 0 //base case: if list is empty, spit length to be zero
+    | x::xs -> 1 + length xs
+
+
+// factorial n == n * (n-1) * (n-2) ...
+// factorial 3 == 3 * 2 * 1 == 6
+let rec factorial n = 
+    if n < 2 then
+        1
+    else
+        n * factorial (n-1)
+
